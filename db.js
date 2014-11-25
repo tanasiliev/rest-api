6b1c9ef5996e596dac0442ab726eeb0149b6beb9
tanasiliev <tanasiliev@gmail.com>
2014-11-25 18:19:06 +0200

    main commit
    running on localhost (still) use cloud db

---
+++ db.js
@@ -0,0 +1,116 @@
+var MongoClient = require('mongodb').MongoClient,
+    Server = require('mongodb').Server;
+    ObjectID = require('mongodb').ObjectID;
+
+var db = null;
+var connectionString = 'mongodb://localhost/rest-api';
+connectionString = 'mongodb://admin:min1@ds055980.mongolab.com:55980/rest-api'
+                   
+MongoClient.connect(connectionString, function(err, mongodb){
+	if(err) throw err;
+	else db = mongodb;
+});
+
+var validateId = function(id){
+    var regex = /^[0-9a-fA-F]{24}$/;
+    return regex.test(id); 
+}
+
+var Collection = function(name){
+     this._collection = db.collection(name);
+}
+
+Collection.prototype = {
+    
+    create : function(obj, callback){
+    	obj.create_dt = new Date().toUTCString();
+	    this._collection.insert(obj, function (err, data) {
+			if (err) callback(err);
+			else callback(null, data);
+		});
+	},
+	select : function(callback){
+	    this._collection.find().toArray(function (err, data) {
+			if (err) callback(err);
+			else callback(null, data);
+		});
+	},
+	update : function(id, obj, callback){
+		var self = this;
+		if(!validateId(id)) {
+			return callback(null, {error: "invalid id"});
+		}	
+	    self._collection.findOne({'_id': ObjectID(id)},function (err, record) {
+			if (err) callback(err);
+			else {
+				if (record){
+					// modify the record
+		        	for (var prop in obj) {
+		        		record[prop] = obj[prop];
+		        	}
+		        	record.updated_dt = new Date().toUTCString(); 
+		            self._collection.save(record, function(err, result) { 
+		            	if (err) callback(err)
+		            	else {
+		            		var returnObj = {};
+		            	    returnObj[id] = 'updated';
+		            	 	callback(null, returnObj);
+		            	}	
+		            });
+			    }
+			    else {
+			    	callback(null, {});
+			    } 
+			}
+		});
+	},
+	remove : function(id, callback){
+		if(!validateId(id)) {
+			return callback(null, {error: "invalid id"});
+		}
+	    this._collection.remove({'_id':ObjectID(id)}, function(err,record) { 
+	    	if (err) callback(err)
+	    	else {
+                var returnObj = {};
+                (record == 0) || (returnObj[id] = 'deleted');
+        	 	callback(null, returnObj);
+	    	}
+	    }); 
+    },
+    selectById : function(id, callback){
+    	if(!validateId(id)) {
+			return callback(null, {error: "invalid id"});
+		}
+	    this._collection.findOne({'_id': ObjectID(id)},function (err, record) {
+			if (err) callback(err)
+		    else {
+			    callback(null, record || {});
+		    }
+		});
+	}
+};
+
+// do small performance if we already have the colection 
+// just return the inctance instead of creating the same new one
+var collections = (function(){
+	var collections = {};
+	var get = function(name){
+	    if(!name){ 
+	      throw 'Name of the Collection is required!';
+	    }
+	    if(!collections[name]){
+	       collections[name] = new Collection(name);
+	    }
+	    return collections[name]; 
+	};
+
+	return {
+		get: get
+	}
+})();
+
+module.exports = {
+	getCollection: function(name){
+        return collections.get(name);;
+	}
+};
---
+++ server.js
@@ -0,0 +1,62 @@
+var express    = require('express'); 		
+var app        = express(); 				
+var bodyParser = require('body-parser');
+var db = require('./db');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 3000; 		
+var router = express.Router(); 			
+
+router.route('/api/:collectionName')
+      
+    .get(function(req, res) {
+    	var params = req.params;   
+    	var collection = db.getCollection(params.collectionName);
+        collection.select(function(err, result){
+			res.json(result);
+    	});
+	})  
+
+	.post(function(req, res) {
+		var params = req.params;   
+        var data = req.body;
+    	var collection = db.getCollection(params.collectionName);
+        collection.create( data, function(err, result){
+			res.json(result);
+    	});
+	});
+
+router.route('/api/:collectionName/:id') 
+
+	.get(function(req, res) {
+        var params = req.params;   
+    	var collection = db.getCollection(params.collectionName);
+        collection.selectById(params.id, function(err, result){
+			res.json(result);
+    	});
+	})
+
+	.put(function(req, res) {
+        var params = req.params;   
+        var data = req.body;
+    	var collection = db.getCollection(params.collectionName);
+        collection.update(params.id, data, function(err, result){
+			res.json(result);
+    	});
+	})
+
+	.delete(function(req, res) {
+	    var params = req.params;   
+    	var collection = db.getCollection(params.collectionName);
+        collection.remove(params.id, function(err, result){
+			res.json(result);
+    	});
+	});
+
+
+app.use('/', router);
+
+app.listen(port);
+//console.log('Server running on port ' + port);
