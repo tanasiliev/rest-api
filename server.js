244527faf5390d2195f979fe3248552e1bbbd7ca
tanasiliev <tanasiliev@gmail.com>
2014-12-19 23:27:09 +0200

    enable cross-origin resource sharing done

--- server.js
+++ server.js
@@ -7,11 +7,17 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || 3000; 		
-var router = express.Router(); 
-	
+var router = express.Router(); 	
+
+
 // enable cross-origin resource sharing
 app.response.__proto__.allowCrossOrigin = function() {
      this.set('Access-Control-Allow-Origin', '*');
+}
+
+var send = function(err, result){
+    this.allowCrossOrigin();
+    this.json(result); 
 } 		
 
 router.route('/api/:collectionName')
@@ -19,18 +25,13 @@ router.route('/api/:collectionName')
     .get(function(req, res) {
     	var params = req.params;   
     	var collection = db.getCollection(params.collectionName);
-        collection.select(function(err, result){
-			res.json(result);
-    	});
+        collection.select(send.bind(res));
 	})  
-
 	.post(function(req, res) {
 		var params = req.params;   
         var data = req.body;
     	var collection = db.getCollection(params.collectionName);
-        collection.create( data, function(err, result){
-			res.json(result);
-    	});
+        collection.create( data, send.bind(res));
 	});
 
 router.route('/api/:collectionName/:id') 
@@ -38,30 +39,20 @@ router.route('/api/:collectionName/:id')
 	.get(function(req, res) {
         var params = req.params;   
     	var collection = db.getCollection(params.collectionName);
-        collection.selectById(params.id, function(err, result){
-			res.json(result);
-    	});
+        collection.selectById(params.id, send.bind(res));
 	})
-
 	.put(function(req, res) {
         var params = req.params;   
         var data = req.body;
     	var collection = db.getCollection(params.collectionName);
-        collection.update(params.id, data, function(err, result){
-			res.json(result);
-    	});
+        collection.update(params.id, data, send.bind(res));
 	})
-
 	.delete(function(req, res) {
 	    var params = req.params;   
     	var collection = db.getCollection(params.collectionName);
-        collection.remove(params.id, function(err, result){
-			res.json(result);
-    	});
+        collection.remove(params.id, send.bind(res));
 	});
 
-
 app.use('/', router);
-
 app.listen(port);
 //console.log('Server running on port ' + port);
