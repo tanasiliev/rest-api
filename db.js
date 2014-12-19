963e516962aec07ee8968d3c2482084d5b58ae16
tanasiliev <tanasiliev@gmail.com>
2014-12-19 23:38:58 +0200

    reindent code

--- db.js
+++ db.js
@@ -1,35 +1,35 @@
 var MongoClient = require('mongodb').MongoClient,
-    Server = require('mongodb').Server;
-    ObjectID = require('mongodb').ObjectID;
+Server = require('mongodb').Server;
+ObjectID = require('mongodb').ObjectID;
 
 var db = null;
 var connectionString = 'mongodb://admin:min1@ds055980.mongolab.com:55980/rest-api'
-                
+
 MongoClient.connect(connectionString, function(err, mongodb){
 	if(err) throw err;
 	else db = mongodb;
 });
 
 var validateId = function(id){
-    var regex = /^[0-9a-fA-F]{24}$/;
-    return regex.test(id); 
+	var regex = /^[0-9a-fA-F]{24}$/;
+	return regex.test(id); 
 }
 
 var Collection = function(name){
-     this._collection = db.collection(name);
+	this._collection = db.collection(name);
 }
 
 Collection.prototype = {
-    
-    create : function(obj, callback){
-    	obj.create_dt = new Date().toUTCString();
-	    this._collection.insert(obj, function (err, data) {
+	
+	create : function(obj, callback){
+		obj.create_dt = new Date().toUTCString();
+		this._collection.insert(obj, function (err, data) {
 			if (err) callback(err);
 			else callback(null, data);
 		});
 	},
 	select : function(callback){
-	    this._collection.find().toArray(function (err, data) {
+		this._collection.find().toArray(function (err, data) {
 			if (err) callback(err);
 			else callback(null, data);
 		});
@@ -39,27 +39,27 @@ Collection.prototype = {
 		if(!validateId(id)) {
 			return callback(null, {error: "invalid id"});
 		}	
-	    self._collection.findOne({'_id': ObjectID(id)},function (err, record) {
+		self._collection.findOne({'_id': ObjectID(id)},function (err, record) {
 			if (err) callback(err);
 			else {
 				if (record){
 					// modify the record
-		        	for (var prop in obj) {
-		        		record[prop] = obj[prop];
-		        	}
-		        	record.updated_dt = new Date().toUTCString(); 
-		            self._collection.save(record, function(err, result) { 
-		            	if (err) callback(err)
-		            	else {
-		            		var returnObj = {};
-		            	    returnObj[id] = 'updated';
-		            	 	callback(null, returnObj);
-		            	}	
-		            });
-			    }
-			    else {
-			    	callback(null, {});
-			    } 
+					for (var prop in obj) {
+						record[prop] = obj[prop];
+					}
+					record.updated_dt = new Date().toUTCString(); 
+					self._collection.save(record, function(err, result) { 
+						if (err) callback(err)
+							else {
+								var returnObj = {};
+								returnObj[id] = 'updated';
+								callback(null, returnObj);
+							}	
+						});
+				}
+				else {
+					callback(null, {});
+				} 
 			}
 		});
 	},
@@ -67,25 +67,25 @@ Collection.prototype = {
 		if(!validateId(id)) {
 			return callback(null, {error: "invalid id"});
 		}
-	    this._collection.remove({'_id':ObjectID(id)}, function(err,record) { 
-	    	if (err) callback(err)
-	    	else {
-                var returnObj = {};
-                (record == 0) || (returnObj[id] = 'deleted');
-        	 	callback(null, returnObj);
-	    	}
-	    }); 
-    },
-    selectById : function(id, callback){
-    	if(!validateId(id)) {
+		this._collection.remove({'_id':ObjectID(id)}, function(err,record) { 
+			if (err) callback(err)
+				else {
+					var returnObj = {};
+					(record == 0) || (returnObj[id] = 'deleted');
+					callback(null, returnObj);
+				}
+			}); 
+	},
+	selectById : function(id, callback){
+		if(!validateId(id)) {
 			return callback(null, {error: "invalid id"});
 		}
-	    this._collection.findOne({'_id': ObjectID(id)},function (err, record) {
+		this._collection.findOne({'_id': ObjectID(id)},function (err, record) {
 			if (err) callback(err)
-		    else {
-			    callback(null, record || {});
-		    }
-		});
+				else {
+					callback(null, record || {});
+				}
+			});
 	}
 };
 
@@ -94,13 +94,13 @@ Collection.prototype = {
 var collections = (function(){
 	var collections = {};
 	var get = function(name){
-	    if(!name){ 
-	      throw 'Name of the Collection is required!';
-	    }
-	    if(!collections[name]){
-	       collections[name] = new Collection(name);
-	    }
-	    return collections[name]; 
+		if(!name){ 
+			throw 'Name of the Collection is required!';
+		}
+		if(!collections[name]){
+			collections[name] = new Collection(name);
+		}
+		return collections[name]; 
 	};
 
 	return {
@@ -110,6 +110,6 @@ var collections = (function(){
 
 module.exports = {
 	getCollection: function(name){
-        return collections.get(name);;
+		return collections.get(name);;
 	}
 };
--- server.js
+++ server.js
@@ -12,49 +12,49 @@ var router = express.Router();
 
 // enable cross-origin resource sharing
 app.response.__proto__.allowCrossOrigin = function() {
-     this.set('Access-Control-Allow-Origin', '*');
+   this.set('Access-Control-Allow-Origin', '*');
 }
 
 var send = function(err, result){
     this.allowCrossOrigin();
-	if (err) {
-	   return this.json({err: err}); 
-	}
+    if (err) {
+        return this.json({err: err}); 
+    }
     this.json(result); 
 } 		
 
 router.route('/api/:collectionName')
-      
-    .get(function(req, res) {
-    	var params = req.params;   
-    	var collection = db.getCollection(params.collectionName);
-        collection.select(send.bind(res));
-	})  
-	.post(function(req, res) {
-		var params = req.params;   
-        var data = req.body;
-    	var collection = db.getCollection(params.collectionName);
-        collection.create( data, send.bind(res));
-	});
+
+.get(function(req, res) {
+    var params = req.params;   
+    var collection = db.getCollection(params.collectionName);
+    collection.select(send.bind(res));
+})  
+.post(function(req, res) {
+    var params = req.params;   
+    var data = req.body;
+    var collection = db.getCollection(params.collectionName);
+    collection.create( data, send.bind(res));
+});
 
 router.route('/api/:collectionName/:id') 
 
-	.get(function(req, res) {
-        var params = req.params;   
-    	var collection = db.getCollection(params.collectionName);
-        collection.selectById(params.id, send.bind(res));
-	})
-	.put(function(req, res) {
-        var params = req.params;   
-        var data = req.body;
-    	var collection = db.getCollection(params.collectionName);
-        collection.update(params.id, data, send.bind(res));
-	})
-	.delete(function(req, res) {
-	    var params = req.params;   
-    	var collection = db.getCollection(params.collectionName);
-        collection.remove(params.id, send.bind(res));
-	});
+.get(function(req, res) {
+    var params = req.params;   
+    var collection = db.getCollection(params.collectionName);
+    collection.selectById(params.id, send.bind(res));
+})
+.put(function(req, res) {
+    var params = req.params;   
+    var data = req.body;
+    var collection = db.getCollection(params.collectionName);
+    collection.update(params.id, data, send.bind(res));
+})
+.delete(function(req, res) {
+    var params = req.params;   
+    var collection = db.getCollection(params.collectionName);
+    collection.remove(params.id, send.bind(res));
+});
 
 app.use('/', router);
 app.listen(port);
