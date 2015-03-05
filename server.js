4791b6a320bc1e18c9693e3949a94158168cee75
Atanas Iliev <tanasiliev@gmail.com>
2015-03-06 00:27:13 +0200

    Update server.js

--- server.js
+++ server.js
@@ -3,20 +3,21 @@ var app        = express();
 var bodyParser = require('body-parser');
 var db = require('./db');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
 var port = process.env.PORT || 3000; 		
 var router = express.Router(); 	
 
-
 // enable cross-origin resource sharing
-app.response.__proto__.allowCrossOrigin = function() {
-   this.set('Access-Control-Allow-Origin', '*');
-}
+var enableCORS = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    next();
+};
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(enableCORS);
 
 var send = function(err, result){
-    this.allowCrossOrigin();
     if (err) {
         return this.json({err: err}); 
     }
